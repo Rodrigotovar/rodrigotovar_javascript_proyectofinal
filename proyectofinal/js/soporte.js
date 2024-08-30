36aab39d925a8750e2b8c3f4f6bffbbba2b68e1d
Rodrigotovar <rodrigotovarpaura@gmail.com>
2024-08-29 18:54:52 -0600

    v.0.0.4
    Debugging a soporte

--- proyectofinal/js/soporte.js
+++ proyectofinal/js/soporte.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Obtén el formulario y añade un manejador para el evento submit
     const form = document.getElementById('comment-form');
     
     form.addEventListener('submit', (event) => {
-        event.preventDefault(); // Previene el envío normal del formulario
+        event.preventDefault(); 
 
         // Mostrar mensaje usando Toastify
         Toastify({
@@ -15,7 +14,6 @@ document.addEventListener('DOMContentLoaded', () => {
             stopOnFocus: true, // Previene que el toast desaparezca al pasar el ratón sobre él
         }).showToast();
 
-        // Opcional: puedes resetear el formulario aquí si lo deseas
         form.reset();
     });
 });
